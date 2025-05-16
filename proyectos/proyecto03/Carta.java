25b57667f221d5a32b19a37308bce0c0de2f5f46
erick <erickmalvaez@ciencias.unam.mx>
2025-05-15 20:38:42 -0600

    Agrego la baraja y el objeto carta

---
+++ proyectos/proyecto03/Carta.java
@@ -0,0 +1,13 @@
+public class Carta{
+    boolean morado, unOjo, banana, alto, pelo;
+    Carta siguienteCarta;
+    
+    public Carta(boolean morado, boolean unOjo, boolean banana, boolean alto, boolean pelo){
+        this.morado = morado;
+        this.unOjo = unOjo;
+        this.banana = banana;
+        this.alto = alto;
+        this.pelo = pelo;
+    }
+
+}
